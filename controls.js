2bda620820306ec6c8f5d837cea9ccc66750e1d5
JorgeHerrada <jorge.herrada.28@gmail.com>
2024-07-11 14:32:48 -0600

    feat: add physics

--- controls.js
+++ controls.js
@@ -9,40 +9,37 @@ class Controls{
     }
 
     #addKeyboardListeners(){
-        document.onkeydown = (event) => {
+        document.onkeydown=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
-                    this.left = true;
+                    this.left=true;
                     break;
                 case "ArrowRight":
-                    this.right = true;
+                    this.right=true;
                     break;
                 case "ArrowUp":
-                    this.forward = true;
+                    this.forward=true;
                     break;
                 case "ArrowDown":
-                    this.reverse = true;
+                    this.reverse=true;
                     break;
             }
-            console.table(this);
         }
-        
-        document.onkeyup = (event) => {
+        document.onkeyup=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
-                    this.left = false;
+                    this.left=false;
                     break;
                 case "ArrowRight":
-                    this.right = false;
+                    this.right=false;
                     break;
                 case "ArrowUp":
-                    this.forward = false;
+                    this.forward=false;
                     break;
                 case "ArrowDown":
-                    this.reverse = false;
+                    this.reverse=false;
                     break;
             }
-            console.table(this);
         }
     }
 }
